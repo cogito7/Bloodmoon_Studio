cc4f7412662c26e096d952ca02d2a55d868e7ff3
bmucf <br407069@ucf.edu>
2025-03-10 21:18:36 -0400

    added some variables like health and damage

---
+++ Assets/Scripts/DamageDealer.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageDealer : MonoBehaviour
+{
+    public int myType;
+    public int powerLevel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        {
+            if (myType == 0)
+            {
+                //I am the player
+                powerLevel = 1;
+            }
+            else if (myType == 1)
+            {
+                //I am the enemy
+                powerLevel = 2;
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.transform.tag == "enemy")
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/HealthManager.cs
+++ Assets/Scripts/HealthManager.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EntityHealth : MonoBehaviour
+public class HealthManager : MonoBehaviour
 {
+    public int startHealth;
+    public int currentHealth;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentHealth = startHealth;
     }
 
     // Update is called once per frame
