e208a71207b78418ed0291ff92eeeab7c5bcba29
bmucf <br407069@ucf.edu>
2025-03-10 20:01:03 -0400

    added a health script and damage script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public float timeRemaining = 91f;
+
+    public TextMeshProUGUI healthDisplay;
+    public TextMeshProUGUI scoreDisplay;
+    public TextMeshProUGUI timerDisplay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimerUpdate();
+    }
+
+    public void HealthUpdate()
+    {
+
+    }
+
+    public void ScoreUpdate()
+    {
+
+    }
+
+    public void TimerUpdate()
+    {
+        //if (m_IsPlayerAtExit || m_IsPlayerCaught)
+        //    return;
+
+        timeRemaining -= Time.deltaTime;
+        int minutes = Mathf.FloorToInt(timeRemaining / 60);
+        int seconds = Mathf.FloorToInt(timeRemaining % 60);
+
+        timerDisplay.text = string.Format("TIME: {0:00}:{1:00}", minutes, seconds);
+    }
+}
